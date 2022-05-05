174f3809b92132967185e82ebf63a46b970dd364
tuyucheng <2759709304@qq.com>
2022-05-06 03:37:05 +0800

    :sparkles: add module maven-dependency

---
+++ maven-dependency/src/main/java/cn/tuyucheng/taketoday/Main.java
@@ -0,0 +1,10 @@
+package cn.tuyucheng.taketoday;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+  public static void main(String[] args) {
+    StringUtils.isBlank(" ");
+  }
+}
